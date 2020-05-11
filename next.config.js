12cdd84219c8e858bc69c79a9c5d55ed020117be
RagOfJoes <vhsvragojos@gmail.com>
2020-05-11 02:25:52 -0700

    ADD: gh-pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 // next.config.js
 process.env.NODE_END !== 'production' && require('dotenv/config');
+const debug = process.env.NODE_ENV !== 'production';
 
 module.exports = {
+	assetPrefix: !debug ? '/covid-19-tracker' : '',
 	env: {
 		PORT: process.env.PORT,
 		API_ENDPOINT: process.env.API_ENDPOINT,
